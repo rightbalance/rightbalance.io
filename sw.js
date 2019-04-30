d181db6547a8a01e2987b641a8644efc52e438d5
Vladimir Pavlov <vovavc@gmail.com>
2019-04-30 19:18:09 +0300

    Updates

--- sw.js
+++ sw.js
@@ -2,6 +2,10 @@ importScripts('https://rightbalance.github.io/rightbalance.io/workbox.4c4f5ca6.j
 
 workbox.precaching.precacheAndRoute([
   {
+    "url": "https://rightbalance.github.io/rightbalance.io/1a404a9ac77580f56122.js",
+    "revision": "02f5b022e4ca3c9e1f9f32f7adbf136f"
+  },
+  {
     "url": "https://rightbalance.github.io/rightbalance.io/34ea81211374727ba4cd.js",
     "revision": "691d2e35bc80914734b2318c0e60ee35"
   },
@@ -10,16 +14,12 @@ workbox.precaching.precacheAndRoute([
     "revision": "35fb7bcde7dbf0111eb49bed2b235fcf"
   },
   {
-    "url": "https://rightbalance.github.io/rightbalance.io/6adf419f7f0fe9905d0b.js",
-    "revision": "e7f04db29c8f2ef7d6a6927331f3363d"
-  },
-  {
     "url": "https://rightbalance.github.io/rightbalance.io/8de0cfca096beb26e396.js",
     "revision": "b67f85f2c90242a4de2aa34d9512e921"
   },
   {
-    "url": "https://rightbalance.github.io/rightbalance.io/ee502ae11a7c69ac7abc.js",
-    "revision": "69f7dcb05c49b0d4e111ae688c106405"
+    "url": "https://rightbalance.github.io/rightbalance.io/b92e7fa3d075e57e2761.js",
+    "revision": "1d80e86708d2bf7c4a3abd368cfd4bc3"
   }
 ], {
   "cacheId": "rightbalance.io",
