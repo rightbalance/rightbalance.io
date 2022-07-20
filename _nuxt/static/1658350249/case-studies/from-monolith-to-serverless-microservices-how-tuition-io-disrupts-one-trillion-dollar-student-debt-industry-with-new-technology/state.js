ba04846122dd306b5e6474f08690ba747cec4752
CircleCI <vlad@rightbalance.io>
2022-07-20 20:51:01 +0000

    Updates

---
+++ _nuxt/static/1658350249/case-studies/from-monolith-to-serverless-microservices-how-tuition-io-disrupts-one-trillion-dollar-student-debt-industry-with-new-technology/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1658350249",layout:"default",error:null,serverRendered:true,routePath:"\u002Fcase-studies\u002Ffrom-monolith-to-serverless-microservices-how-tuition-io-disrupts-one-trillion-dollar-student-debt-industry-with-new-technology",config:{}};
